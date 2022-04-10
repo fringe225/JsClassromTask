35dccd1c877328684686069811352bacdc4188b0
fringe225 <turalka@code.edu.az>
2022-04-10 22:34:00 +0400

    Done

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-function Credit(){
-    let sum = document.querySelector('.sum').value;
-    let percentage = document.querySelector('.percent').value;
-    let month = document.querySelector('.month').value;
+//Bank Credit
 
-    let summary = ((sum*percentage)/100)*(month/12);
-    summary= Number.parseFloat(summary) + Number.parseFloat(sum);
+// function Credit(){
+//     let sum = document.querySelector('.sum').value;
+//     let percentage = document.querySelector('.percent').value;
+//     let month = document.querySelector('.month').value;
 
-    document.querySelector('.summaryPercentage').innerHTML=summary;
-    document.querySelector('.monthPercentage').innerHTML=Number.parseFloat(summary)/Number.parseFloat(month);
+//     let summary = ((sum*percentage)/100)*(month/12);
+//     summary= Number.parseFloat(summary) + Number.parseFloat(sum);
+
+//     document.querySelector('.summaryPercentage').innerHTML=summary;
+//     document.querySelector('.monthPercentage').innerHTML=Number.parseFloat(summary)/Number.parseFloat(month);
 
 
-}
+// }
+
+
+
+
+//Counter Program
+
+let dec = document.querySelector(".decrease");
+let res = document.querySelector(".reset");
+let inc = document.querySelector(".increase");
+
+let change = document.querySelector(".variable");
+
+dec.addEventListener('click',()=>{
+    let num = change.textContent;
+    num--;
+    change.textContent=num;
+});
+
+inc.addEventListener('click',()=>{
+    let num = change.textContent;
+    num++;
+    change.textContent=num;
+});
+
+res.addEventListener('click',()=>{
+    let num = change.textContent;
+    num=0;
+    change.textContent=num;
+});
+
+
